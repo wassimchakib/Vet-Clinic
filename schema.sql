1d69a4b5da5c26e995027d95d4e3dd33c251821a
Wassim <wassimchakib97@gmail.com>
2023-01-16 17:32:48 +0100

    Add id as primary key auto generated'

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 create table animals(
-  id integer,
+  id integer generated always as identity primary key,
   name varchar(100),
   date_of_birth date,
   escape_attempts integer,
